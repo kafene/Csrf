23b5074e45930e1dc73d24b1bf8267547e5a9a1c
kafene <kafene@users.noreply.github.com>
2014-01-30 18:12:30 -0500

    Create example.php

---
+++ example.php
@@ -0,0 +1,45 @@
+session_start();
+$csrf = new \kafene\Csrf();
+$csrf->setRequestParam('custom_csrf_token_param');
+
+try {
+    $csrf->protect();
+    $result = "CSRF check passed or no data sent";
+} catch (\kafene\CsrfException $e) {
+    $result = $e->getMessage() . ' - Form ignored.';
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>CSRF Protection Demo</title>
+    <style>
+        #result {
+            color: #f22;
+            font-weight: bold;
+        }
+    </style>
+<body>
+
+    <h1>CSRF Protection Demo</h1>
+    <pre id="result">
+        <?= $result ?>
+    </pre>
+
+    <form name="csrf_form" action="#" method="post">
+        <h2>Form using generated token.</h2>
+        <?= $csrf->formInput() ?>
+        <input type="text" name="field" value="somevalue">
+        <input type="submit" value="Send form">
+    </form>
+
+    <form name="nocsrf_form" action="#" method="post">
+        <h2>Form simulating a CSRF attack.</h2>
+        <input type="hidden" name="custom_csrf_token_param" value="whateverkey">
+        <input type="text" name="field" value="somevalue">
+        <input type="submit" value="Send form">
+    </form>
+</body>
+</html>
